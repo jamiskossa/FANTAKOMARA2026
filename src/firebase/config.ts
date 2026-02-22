ee4cafeb99d9cbbdd76e6abf1234ed16459e7e93
Jamis Kossa <jamiskossa@gmail.com>
2026-02-22 04:05:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-98021600-d9d31",
+  "appId": "1:853851139350:web:85fdae349ea3d90e0a3437",
+  "apiKey": "AIzaSyD4C8NWeI8_jgOuRRN4xZDyYspYkUNLwlc",
+  "authDomain": "studio-98021600-d9d31.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "853851139350"
+};
